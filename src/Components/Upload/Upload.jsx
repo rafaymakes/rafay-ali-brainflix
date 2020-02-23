8064bd73a29f04532976f1c69a394b948f106ae5
Rafay Ali <rafayny@gmail.com>
2020-02-23 18:01:17 -0500

    upload page fully responsive

--- src/Components/Upload/Upload.jsx
+++ src/Components/Upload/Upload.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './../Header/header.jsx';
+import Uploader from './Uploader/Uploader';
 import './Upload.scss';
 
 const keyAffix = '?api_key=94f19a78-ed3b-4723-9773-164ca4dffeee';
@@ -9,6 +10,7 @@ class Upload extends Component {
         return (
             <div className="App">
                 <Header />
+                <Uploader />
             </div>
         )
     }
